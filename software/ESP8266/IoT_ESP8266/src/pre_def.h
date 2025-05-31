95657fd0fff2a64bc12d4c2ac5d16c8b5a17a636
abbas <a.ghadiri75@gmail.com>
2025-05-31 23:09:13 +0330

    software esp8266, add pre_def.h header, code become complete, now time update every 6 sec and send it through UART, after receive data in esp side, serial print the input and send it to server

---
+++ software/ESP8266/IoT_ESP8266/src/pre_def.h
@@ -0,0 +1,8 @@
+//This header is for portotype functions
+void wificonfig();
+void print_time();
+void print_date();
+void time_update();
+void time_update_event();
+void read_from_serial_and_send();
+void http_post(String input_serial);
