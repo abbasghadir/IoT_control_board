345fdc92260092a69cd9b4475cd33266d32ceda1
abbas <a.ghadiri75@gmail.com>
2025-06-07 18:59:15 +0330

    change some functions in ESP8266

--- software/ESP8266/IoT_ESP8266/src/main.cpp
+++ software/ESP8266/IoT_ESP8266/src/main.cpp
@@ -11,8 +11,9 @@ const char* endpoint_url = "http://192.168.1.5/IoT_input_detection.php";
 unsigned long last_time = 0;
 
 //WiFi setting
-const char *ssid = "HA6400_9F1A";
-const char *password = "5fa845f3bgas";
+const char *ssid = "SSID";
+const char *password = "PASSWORD";
+const char* time_sync_cmd = "successfully";
 
 WiFiUDP ntpUDP;
 
@@ -33,24 +34,12 @@ void setup() {
 
 void loop() {
   time_update_event();
-  read_from_serial_and_send();
+  if (uart_reader() != "N")
+  {
+    http_post(uart_reader());
+  }
 }
 
-void print_time(){
-  Serial.print("Time: ");
-  Serial.println(NTP_timeClient.getFormattedTime());
-}
-
-void print_date(){
-  unsigned long epochTime = NTP_timeClient.getEpochTime();
-  time_t rawtime = epochTime;
-  struct tm * ti = localtime(&rawtime);
-  char dateStr[12];
-  snprintf(dateStr, sizeof(dateStr), "%04d-%02d-%02d", 
-           ti->tm_year + 1900, ti->tm_mon + 1, ti->tm_mday);
-  Serial.print("Date: ");
-  Serial.println(dateStr);
-}
 void wificonfig(){
   printf("Connecting to: %s\n", ssid);
   WiFi.begin(ssid,password);
@@ -64,46 +53,42 @@ void wificonfig(){
   Serial.println(WiFi.localIP());  
 }
 
-void time_update_event(){
-  if (millis() - last_time >= ntp_update_time)
-  {
-    time_update();    
-  }
-  //update NTP time
-  
-  //set rtc by ntp epoch time
-  // RTC = NTP_timeClient.getEpochTime(); //for rtc
-
-  // struct timeval tv = {.tv_sec = RTC, .tv_usec = 0}; //for rtc
-  // settimeofday(&tv, NULL); //for rtc
-}
-
-void time_update(){
-  if (NTP_timeClient.update())
-  {
-    print_time();
-    print_date();  
-    last_time = millis();
-  }else system_restart();    
-  //update NTP time
-  
-  //set rtc by ntp epoch time
-  // RTC = NTP_timeClient.getEpochTime(); //for rtc
-
-  // struct timeval tv = {.tv_sec = RTC, .tv_usec = 0}; //for rtc
-  // settimeofday(&tv, NULL); //for rtc
-}
-
-void read_from_serial_and_send(){
+String uart_reader(){
   if (Serial.available())
   {
     String input_serial= Serial.readStringUntil('\n');
     Serial.print("Responce: ");
     Serial.println(input_serial);
-    http_post(input_serial);
+    return input_serial;
+  }else return "N";
+}
+
+void time_update_event(){
+  if (millis() - last_time >= ntp_update_time)
+  {
+    time_update();    
   }
 }
 
+void time_update(){
+  Serial.println("Waiting for NTP update");
+  unsigned long epochTime;
+  while (1)
+  {
+    if (NTP_timeClient.update())
+    {
+      epochTime = NTP_timeClient.getEpochTime();
+      Serial.print("epoch:");
+      Serial.println(epochTime);
+      delay(100);
+      if (uart_reader() == time_sync_cmd)
+      {
+        last_time = millis();
+        break;
+      }
+    }else system_restart();
+  }
+}
 void http_post(String input_serial){
   JsonDocument doc;
   doc["Result"] = input_serial;
